72f931cc8c05cab9bb10582627c7a0a5d7bbcc41
maks19801 <49532728+maks19801@users.noreply.github.com>
2022-01-12 10:29:14 +0200

    Update StorageService.cs

--- CommonServices/StorageService.cs
+++ CommonServices/StorageService.cs
@@ -21,11 +21,11 @@ namespace TableSearch.Services
 
         public StorageService()
         {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(@"DefaultEndpointsProtocol=https;AccountName=porkhovskyi;AccountKey=SzTqiO7s0CGnO80+3pzbCW9GT6nFDrIfFUpvMHIK4lhYx7j92SpxNQ+tmKfJdImmBROTBi8qyaJJDTmmA4iE8Q==;BlobEndpoint=https://porkhovskyi.blob.core.windows.net/;QueueEndpoint=https://porkhovskyi.queue.core.windows.net/;TableEndpoint=https://porkhovskyi.table.core.windows.net/;FileEndpoint=https://porkhovskyi.file.core.windows.net/;");
-            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(@"DefaultEndpointsProtocol=https;AccountName=itstep1511gen;AccountKey=dRV5C8hAj+QpKYW+7My5xzpd1C5ZLSmWs6Wgnj/sQnaUR670ofKPcOFOXs7EEh0GUyXgrnHgpPFUqKeBBhYkhA==;BlobEndpoint=https://itstep1511gen.blob.core.windows.net/;QueueEndpoint=https://itstep1511gen.queue.core.windows.net/;TableEndpoint=https://itstep1511gen.table.core.windows.net/;FileEndpoint=https://itstep1511gen.file.core.windows.net/;");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(@"connection string");
+            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(@"connection string");
 
 #if CONSUMER
-            Queue1 = new QueueClient(@"DefaultEndpointsProtocol=https;AccountName=porkhovskyi;AccountKey=SzTqiO7s0CGnO80+3pzbCW9GT6nFDrIfFUpvMHIK4lhYx7j92SpxNQ+tmKfJdImmBROTBi8qyaJJDTmmA4iE8Q==;BlobEndpoint=https://porkhovskyi.blob.core.windows.net/;QueueEndpoint=https://porkhovskyi.queue.core.windows.net/;TableEndpoint=https://porkhovskyi.table.core.windows.net/;FileEndpoint=https://porkhovskyi.file.core.windows.net/;", "queue1");
+            Queue1 = new QueueClient(@"connection string", "queue1");
             var queueCreationTask = Queue1.CreateIfNotExistsAsync();
 #endif
 
