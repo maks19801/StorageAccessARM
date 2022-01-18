0c2f0934acbed07911302c90b9cd1ff2ef8567e1
Порховский Максим <maks19801@gmail.com>
2022-01-18 21:28:18 +0200

    blob access

--- BlobAccess/Program.cs
+++ BlobAccess/Program.cs
@@ -1,5 +1,10 @@
-﻿using Azure.Storage.Blobs;
-
+﻿using Azure;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,18 +16,46 @@ namespace BlobAccess
 {
     class Program
     {
+       private static string accountName = "porkhovskyi";
+       private static string accountKey = "SzTqiO7s0CGnO80+3pzbCW9GT6nFDrIfFUpvMHIK4lhYx7j92SpxNQ+tmKfJdImmBROTBi8qyaJJDTmmA4iE8Q==";
+        private static string containerName = "my-container";
         static void Main(string[] args)
         {
-            BlobServiceClient serviceClient = new BlobServiceClient(@"DefaultEndpointsProtocol=https;AccountName=itstep1511blob;AccountKey=ucnFo5pw6zUUlxb1cParY2D+qNX7NVgyUmawYX0Sy2TxGli/IydNewcEBui+3EvQHp7pFXxeMaJ6Xt/Zf7ItFw==;BlobEndpoint=https://itstep1511blob.blob.core.windows.net/;");
-            BlobContainerClient containerClient = serviceClient.GetBlobContainerClient("container1");
-
-            foreach (var blob in containerClient.GetBlobs())
-            {
-                var reference = containerClient.GetBlobClient(blob.Name);
-                reference.DownloadTo(blob.Name);
-            }
-
+           
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(containerName);
+            var blobs = container.ListBlobs();
+            DownLoadBlobs(blobs);
+            Console.WriteLine("Input this file name and extention(EN-EURUSD-20220113-M.pdf): ");//EN-EURUSD-20220113-M.pdf
+            string fileName = Console.ReadLine();
+            Console.WriteLine("This is you URI to get file: " + GetFileUri(fileName));
             Console.ReadKey();
+        }
+
+        private static void DownLoadBlobs(IEnumerable<IListBlobItem> blobs)
+        {
+            foreach (var blob in blobs)
+            {
+                if (blob is CloudBlockBlob blockBlob)
+                {
+                    blockBlob.DownloadToFile(blockBlob.Name, FileMode.Create);
+                }
+                else if (blob is CloudBlobDirectory blobDirectory)
+                {
+                    Directory.CreateDirectory(blobDirectory.Prefix);
+                    DownLoadBlobs(blobDirectory.ListBlobs());
+                }
+
+            }
+        }
+        private static Uri GetFileUri(string fileName)
+        {
+            
+           
+            UriBuilder fileUri = new UriBuilder($"https://{accountName}.blob.core.windows.net/{containerName}/{fileName}");
+           
+            return fileUri.Uri;
         }
     }
 }
